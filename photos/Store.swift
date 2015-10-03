e08f8db32a2273c5e3facfbeefae750b8e324e9f
Jon Brennecke <jpbrennecke@gmail.com>
2015-10-03 12:45:54 -0700

    Adding stores and observable helper classes

---
+++ photos/Store.swift
@@ -0,0 +1,13 @@
+//
+//  Store.swift
+//  photos
+//
+//  Created by Jonathan Brennecke on 10/3/15.
+//  Copyright © 2015 Brett Berry. All rights reserved.
+//
+
+import Foundation
+
+class Store: Observable {
+    
+}
